7f814df0b1b7c83c1d06856cc6a8198ad90d8ee6
Shazia Parveen <162968658+parveenshazia123@users.noreply.github.com>
2024-07-22 17:45:18 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-let favNumber : number = 3;
-console.log(`My favourite number is ${favNumber},`);
-
+let names : string[] = ["Muniza","Saima","Rubina","Saba"];
+let message : string = "Wouid you like to learn some Typescript?";
+console.log(names[0] + "    " + message)
+console.log(names[1] + "    " + message)
+console.log(names[2] + "    " + message)
+console.log(names[3] + "    " + message)
